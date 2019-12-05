14f1b403eb8de578e0831a44772feb010c6040ab
juliehauge <54266490+juliehauge@users.noreply.github.com>
2019-12-05 11:15:08 +0100

    Updates

---
+++ precache-manifest.1ec69402487a2c3f921d6807a9fbdc92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99f3b2805c8efe550713c8f7252593cd",
+    "url": "/temaoppgave-4/index.html"
+  },
+  {
+    "revision": "b966c0f39622a5a829fa",
+    "url": "/temaoppgave-4/static/css/main.4cf62ef3.chunk.css"
+  },
+  {
+    "revision": "1b44123d4c20d62b804b",
+    "url": "/temaoppgave-4/static/js/2.0699aed9.chunk.js"
+  },
+  {
+    "revision": "b966c0f39622a5a829fa",
+    "url": "/temaoppgave-4/static/js/main.30758d89.chunk.js"
+  },
+  {
+    "revision": "93568e454fdfc7917f47",
+    "url": "/temaoppgave-4/static/js/runtime-main.ea949348.js"
+  }
+]);
